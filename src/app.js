355ee63c9d0a94d2396b4176c737eaada26a7a8d
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-07-12 17:56:51 -0500

    feat(app):add parsers to requests body

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
 const express = require("express");
-const productsRouter = require('./routes/test.routes')
+const authRoutes = require('./routes/auth.routes')
+
+
 const cors =  require('cors')
 const {join} = require('path')
 const app = express();
 
 app.use(cors({
-    origin:[]
+    origin:["*"]
 }))
-
-app.user(static(pat))
+app.use(express.urlencoded())
+app.use(express.json())
+app.use(express.static(join(__dirname,'public')))
 app.get('/',(req,res)=>{
     res.json({message:"Welcome to server, this server is for practice mongoose !!"})
 })
-app.use(loginRouts)
-app.use(productsRouter)
+app.use('/auth/',authRoutes)
+
 
 
 module.exports = app;
