5b26f29c705925da1615d90228d711da9c69b5c9
Jose David Suarez Cardona <josedavid4227@gmail.com>
2023-08-02 14:34:25 -0500

    featapi): Implementing dependency injection

--- src/database/users.Service.js
+++ src/database/users.Service.js
@@ -1,21 +1,21 @@
-const userModel = require("./models/userScheme");
+
 
 const getUsersWithComments = async () => {
   const users = await userModel.find();
   return users;
 };
 const getUserByEmail = async (email) => {
-  const user = await userModel.findOne({email});
+  const user = await userModel.findOne({ email });
   return user;
 };
-const creatNewUser = async (data)=>{
-  const new_user =  new userModel({
-    name:data.name,
-    email:data.email,
-    url_avatar:data.picture
+const creatNewUser = async (data) => {
+  const new_user = new userModel({
+    name: data.name,
+    email: data.email,
+    url_avatar: data.picture
   })
   console.log(new_user);
   await new_user.save()
   return new_user
 }
-module.exports = { getUsersWithComments, getUserByEmail,creatNewUser};
+module.exports = { getUsersWithComments, getUserByEmail, creatNewUser };
